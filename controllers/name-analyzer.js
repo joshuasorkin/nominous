276765f2f9c2c14e087235b1a9158a49b3bf6d29
joshua sorkin <joshua.d.sorkin@gmail.com>
2021-11-16 12:12:21 -0800

    add name-analyzer class with md5 hashing

--- controllers/name-analyzer.js
+++ controllers/name-analyzer.js
@@ -1,5 +1,11 @@
+const md5 = require('blueimp-md5');
+
 class NameAnalyzer{
 
+    //gets the MD5 hash of a string passed in
+    getHash(name){
+        return md5(name);
+    }
 }
 
 module.exports = NameAnalyzer;
