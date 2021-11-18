bc4033acb07907a4c1d35af035178251e6804b2f
joshua sorkin <joshua.d.sorkin@gmail.com>
2021-11-18 15:14:34 -0800

    more changes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const soundtestHTML = '../public/javascripts/soundtest.html';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send(`Welcome to Nominous, the Name Analyzer!  To use the API, make a GET request to `
-  +`${req.headers.host + req.url}/api/[your first name]/[your birth year].`)
+  +`${req.headers.host + req.url}api/[your first name]/[your birth year].`)
 });
 
 module.exports = router;
