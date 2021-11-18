9393d55404181d757672fefb1adbf5738141d139
joshua sorkin <joshua.d.sorkin@gmail.com>
2021-11-18 15:09:12 -0800

    change result for index page

--- routes/api.js
+++ routes/api.js
@@ -4,33 +4,42 @@ const yearRegex = /^[1-9]\d+$/
 const ContentCreator = require('../controllers/content-creator');
 const contentCreator = new ContentCreator();
 
-router.get('/:name/:birthyear', function(req, res, next) {
-  let name = req.params.name;
-  let birthyear = req.params.birthyear;
-  let output;
-  let currentYear = new Date().getFullYear();
+router.get('/analysis/:name/:birthyear', function(req, res, next) {
+    //get parameters from request
+    let name = req.params.name;
+    let birthyear = req.params.birthyear;
 
-  res.setHeader('Content-Type', 'application/json');
-  if (yearRegex.test(birthyear)){
-      if(birthyear<=currentYear){
-        let outputText = contentCreator.create(name,birthyear);
-        output = {
-            nameAnalysis:outputText
-        }
-      }
-      else{
-          output = {
-              error: `Year must be ${birthyear} or before`
+    let output;
+    let currentYear = new Date().getFullYear();
+
+    //set result to return JSON
+    res.setHeader('Content-Type', 'application/json');
+
+    //check if birth year is a valid year
+    if (yearRegex.test(birthyear)){
+        //check if birth year is current year or earlier
+        if(birthyear<=currentYear){
+            //generate name analysis
+            let outputText = contentCreator.create(name,birthyear);
+            //create JSON output
+            output = {
+                nameAnalysis:outputText
             }
-      }
-  }
-  else{
-      output = {
-          error: `Invalid birth year`
-      }
-  }
+        }
+        else{
+            output = {
+                error: `Year must be ${birthyear} or before`
+                }
+        }
+    }
+    else{
+        output = {
+            error: `Invalid birth year`
+        }
+    }
 
-  res.end(JSON.stringify(output));
+    //send JSON as response
+    res.end(JSON.stringify(output));
 
 
 });
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const soundtestHTML = '../public/javascripts/soundtest.html';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname,soundtestHTML));
+  res.send(`Welcome to Nominous, the Name Analyzer!  To use the API, make a GET request to `
+  +`${req.headers.host + req.url}/api/[your first name]/[your birth year].`)
 });
 
 module.exports = router;
